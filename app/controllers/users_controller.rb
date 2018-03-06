16cdbba6fd5e4fb2d52e1a5a59ecbfe8b3a99b94
Satoshi Okada <soysoysoup@gmail.com>
2018-03-06 05:18:23 +0000

    Close #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    #自分の参加対戦会一覧表示したい。
+    #自分の参加対戦会一覧表示したい
     #対戦会日時で降順でソートしてます
     #参考:https://qiita.com/makitokezuka/items/edca67ed23fa71b90997
     @participate = current_user.participant_managements.joins(:battle).all.order("battles.battle_date DESC")
