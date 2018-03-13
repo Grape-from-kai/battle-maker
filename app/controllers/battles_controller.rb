3b316b1c2143ee5ee5eff3d67ab8a52a7a82cb55
Satoshi Okada <soysoysoup@gmail.com>
2018-03-13 06:54:16 +0000

    Close #5

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -18,14 +18,14 @@ class BattlesController < ApplicationController
     end
   end
     
-  # def create
-  #   @battle = current_user.battles.new(battle_params)
-  #   if @battle.save
-  #     redirect_to battles_path, notice: "対戦会を作成しました！"
-  #   else
-  #     render 'new'
-  #   end
-  # end
+  def create
+    @battle = current_user.battles.new(battle_params)
+    if @battle.save
+      redirect_to battles_path, notice: "対戦会を作成しました！"
+    else
+      render 'new'
+    end
+  end
   
   def show
     @joined = current_user.participant_managements.find_by(battle_id: @battle.id)
