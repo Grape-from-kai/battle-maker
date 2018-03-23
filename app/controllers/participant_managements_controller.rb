1acdcaf2fb3972dea8957f480b83e9cc1ca7d7c9
Satoshi Okada <soysoysoup@gmail.com>
2018-03-23 04:29:06 +0000

    Close #14 and shape codes

--- app/controllers/participant_managements_controller.rb
+++ app/controllers/participant_managements_controller.rb
@@ -1,12 +1,12 @@
 class ParticipantManagementsController < ApplicationController
   def create
-    join = ParticipantManagement.create(battle_id: params[:battle_id],user_id: current_user.id)
-    redirect_to user_path(current_user.id),notice:"「#{join.battle.title}」に参加しました"
+    join = ParticipantManagement.create(battle_id: params[:battle_id], user_id: current_user.id)
+    redirect_to user_path(current_user.id),notice: "「#{join.battle.title}」に参加しました"
   end
 
   def destroy
     cancel = ParticipantManagement.find(params[:id])
     cancel.destroy
-    redirect_to user_path(current_user.id), notice:"「#{cancel.battle.title}」の参加を取り消しました"
+    redirect_to user_path(current_user.id), notice: "「#{cancel.battle.title}」の参加を取り消しました"
   end
 end
